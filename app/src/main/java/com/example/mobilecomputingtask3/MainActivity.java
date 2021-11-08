448d963ea672269717914078b286f4dd0e273d41
usama <bitf18a534@pucit.edu.pk>
2021-11-08 21:04:00 +0500

    intent is creted

--- app/src/main/java/com/example/mobilecomputingtask3/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingtask3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mobilecomputingtask3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         move=findViewById(R.id.button);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+            }
+        });
     }
 }
