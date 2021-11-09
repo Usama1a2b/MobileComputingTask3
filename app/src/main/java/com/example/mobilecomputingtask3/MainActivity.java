441d7062c48badb6bbfa9a607e280513401a28dc
usama <bitf18a534@pucit.edu.pk>
2021-11-09 21:08:07 +0500

    random number generated

--- app/src/main/java/com/example/mobilecomputingtask3/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingtask3/MainActivity.java
@@ -6,15 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button move;
+    TextView text;
     String [] ArabicLetters={"ا","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ظ","ع","غ","ف","ق","ك","ل","م","ن","ه","و","ي"};
+    Random dice=new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int number= dice.nextInt(28);
+        text=findViewById(R.id.textView);
+
         move=findViewById(R.id.button);
         move.setOnClickListener(new View.OnClickListener() {
             @Override
