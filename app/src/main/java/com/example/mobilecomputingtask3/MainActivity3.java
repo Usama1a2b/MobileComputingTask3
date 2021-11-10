9ed5be9025994a7f40f10ec79163157bea1b08d4
usama <bitf18a534@pucit.edu.pk>
2021-11-10 21:18:48 +0500

    btn6

--- app/src/main/java/com/example/mobilecomputingtask3/MainActivity3.java
+++ app/src/main/java/com/example/mobilecomputingtask3/MainActivity3.java
@@ -112,6 +112,25 @@ public class MainActivity3 extends AppCompatActivity {
             }
         });
 
+        btn6=findViewById(R.id.button13);
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(alphabet.equals("ظ") || alphabet.equals("ذ") || alphabet.equals("ث"))
+                {
+                    GlobalVariable.glob++;
+                    intent=new Intent(MainActivity3.this,MainActivity.class);
+                    startActivity(intent);
+
+                }
+                else
+                {
+                    intent=new Intent(MainActivity3.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
 
 
     }
