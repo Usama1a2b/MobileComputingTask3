a11029878ea1eb66ffd17db31cec65fe23663a59
usama <bitf18a534@pucit.edu.pk>
2021-11-11 19:46:18 +0500

    button to url

--- app/src/main/java/com/example/mobilecomputingtask3/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingtask3/MainActivity.java
@@ -7,18 +7,30 @@ package com.example.mobilecomputingtask3;
         import android.content.Intent;
         import android.net.Uri;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button move;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //repository
-        Uri uri = Uri.parse("https://www.youtube.com"); // missing 'http://' will cause crashed
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+        move=findViewById(R.id.button);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                Uri uri = Uri.parse("https://github.com/Usama1a2b/MobileComputingTask3.git"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
